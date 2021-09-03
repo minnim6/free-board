d7938fd44b6c6359d2a1a21a6f5338d7d34e687a
minnim6 <mi_jeong_o@naver.com>
2021-09-03 14:15:55 +0900

    Quesetionboard entity create

--- src/main/java/com/project/petboard/domain/questionboard/QuestionBoard.java
+++ src/main/java/com/project/petboard/domain/questionboard/QuestionBoard.java
@@ -1,4 +1,41 @@
 package com.project.petboard.domain.questionboard;
 
+import com.project.petboard.domain.member.Member;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class QuestionBoard {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long QuestionBoardNumber;
+
+    @ManyToOne
+    @JoinColumn
+    private Member member;
+
+    private String QuestionBoardTitle;
+
+    private String QuestionBoardContents;
+
+    @Temporal(TemporalType.DATE)
+    @Column(insertable = true,updatable = false)
+    @CreationTimestamp
+    private Date QuestionBoardCreateDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(insertable = true,updatable = false)
+    @CreationTimestamp
+    private Date QuestionBoardCommentDate;
+
+    private String QuestionBoardComment;
+
 }
