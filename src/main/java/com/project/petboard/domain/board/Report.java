fc7fbe39f5c2ce55d567fb4f6b8b9f12a5a4cdb1
minnim6 <mi_jeong_o@naver.com>
2021-09-03 14:34:52 +0900

    entity create

--- src/main/java/com/project/petboard/domain/board/Report.java
+++ src/main/java/com/project/petboard/domain/board/Report.java
@@ -1,4 +1,38 @@
 package com.project.petboard.domain.board;
 
+import com.project.petboard.domain.member.Member;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Report {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long ReportNumber;
+
+    @ManyToOne
+    @JoinColumn
+    private Board board;
+
+    private String ReportCategory;
+
+    @ManyToOne
+    @JoinColumn
+    private Member member;
+
+    private String ReportContents;
+
+    @Temporal(TemporalType.DATE)
+    @Column(insertable = true,updatable = false)
+    @CreationTimestamp
+    private Date ReportCreateDate;
+
 }
