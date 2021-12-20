6f9c1410b4c0e85281374e7b20a7d34c3bfdd8fd
mijeong <mijeongo9806@gamail.com>
2021-12-20 12:25:49 +0900

    no message

--- src/test/java/com/project/petboard/dummy/MemberDummy.java
+++ src/test/java/com/project/petboard/dummy/MemberDummy.java
@@ -1,19 +1,15 @@
 package com.project.petboard.dummy;
 
-import com.project.petboard.domain.member.Member;
-import com.project.petboard.domain.member.MemberRepository;
-import com.project.petboard.domain.member.MemberSingupCategory;
+import com.project.petboard.domain.member.MemberSignupCategory;
 import com.project.petboard.domain.member.MemberStatus;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Getter
 public class MemberDummy {
 
     private final String memberNickname = "회원1";
-    private final MemberSingupCategory memberSingupCategory = MemberSingupCategory.KAKAO;
+    private final MemberSignupCategory memberSingupCategory = MemberSignupCategory.KAKAO;
     private final MemberStatus memberStatus = MemberStatus.Y;
 
 }
